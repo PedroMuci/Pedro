0191f4b6d6570f2f3b4bf9ac14a17d8da09c46f1
PedroMuci <142358377+PedroMuci@users.noreply.github.com>
2024-10-04 19:11:41 -0300

    adicionando html 5 e estilo

--- FolhaExercicios/ex10.php
+++ FolhaExercicios/ex10.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex10</title>
+    <style>
+        body {
+            background-color: black;
+            font-family: monospace;
+            color: white;
+            }
+    </style>
+</head>
 <body>
     <?php
 
     $materias = array(
         "bsn" => array(
             "3a Fase" => array(
+                //Na verdade desenvolvimento web é na segunda fase, mas como foi solicitado, deixamos na terceira fase mesmo
                 "desenvWeb",
                 "bancoDados 1",
                 "engSoft 1"
             ),
             "4a Fase" => array(
-                "Intro Web",
+                //No array de exemplo tá intro web, mas como a materia é programação web, eu deixei prog web
+                "Prog Web",
                 "bancoDados 2",
                 "engSoft 2"
             )
