4362e12da42960646653af6a83d428405ec621b6
PedroMuci <142358377+PedroMuci@users.noreply.github.com>
2024-10-04 19:04:02 -0300

    adicionando html 5 e estilo

--- FolhaExercicios/ex06.php
+++ FolhaExercicios/ex06.php
@@ -1,5 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex06</title>
+    <style>
+        body {
+            background-color: black;
+            font-family: monospace;
+            color: white;
+            text-align: center;
+            }
+    </style>
+</head>
 <body>
     <form method="POST">
         Maçã (kg): <input type="number" name="maca"><br>
