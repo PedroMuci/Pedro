16d296aed2a35c982d6d61e924c2343aa8faa80d
PedroMuci <142358377+PedroMuci@users.noreply.github.com>
2024-10-04 19:05:45 -0300

    adicionando html 5 e estilo

--- FolhaExercicios/ex07.php
+++ FolhaExercicios/ex07.php
@@ -1,5 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex07</title>
+    <style>
+        body {
+            background-color: black;
+            font-family: monospace;
+            color: white;
+            text-align: center;
+            }
+    </style>
+</head>
 <body>
     <form method="POST">
         Valor do carro (R$): <input type="number" name="valor_carro" value="22500"><br>
